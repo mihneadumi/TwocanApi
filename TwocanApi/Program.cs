88aaec0e1819fae8dfb82269b1fc048f7559945d
912-Dumitrana-Mihnea <mihnea.dumitrana@gmail.com>
2024-05-14 12:54:52 +0300

    connect to hosted db

--- TwocanApi/Program.cs
+++ TwocanApi/Program.cs
@@ -27,7 +27,7 @@ namespace TwocanApi
 
             builder.Services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection2"));
             });
 
             var app = builder.Build();
